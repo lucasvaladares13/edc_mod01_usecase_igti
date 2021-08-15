18bd980525cdb95499f4a068f8793f40cf22e1ad
lucasvaladares13 <lucasvao96@gmail.com>
2021-08-15 20:44:15 -0300

    Att lambdafunction

--- etl/lambda_function.py
+++ etl/lambda_function.py
@@ -7,7 +7,7 @@ def handler(event, context):
     client = boto3.client('emr', region_name='us-east-2')
 
     cluster_id = client.run_job_flow(
-                Name='EMR-Lucas-IGTI-delta',
+                Name='EMR-lucas-IGTI-delta-01',
                 ServiceRole='EMR_DefaultRole',
                 JobFlowRole='EMR_EC2_DefaultRole',
                 VisibleToAllUsers=True,
@@ -30,7 +30,7 @@ def handler(event, context):
                             'InstanceCount': 1,
                         }
                     ],
-                    'Ec2KeyName': 'lucas-igti-usecase01',
+                    'Ec2KeyName': 'ney-igti-teste',
                     'KeepJobFlowAliveWhenNoSteps': True,
                     'TerminationProtected': False,
                     'Ec2SubnetId': 'subnet-2bd61556'
