309cdf1dba6a6052dd0e226679d9137462b07e63
lucasvaladares13 <lucasvao96@gmail.com>
2021-08-15 20:07:04 -0300

    Att

--- scripts/build_lambda_package.sh
+++ scripts/build_lambda_package.sh
@@ -1,54 +1,51 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash 
 
 cd infrastructure
 
-# Declara variavel para reutilização nas validações do diretorio
+# Declara variavel para reutilização nas validações do diretório
 PACKAGE="package"
 
-# Cria o diretorio e instala as dependencias da função lambda
-if [ -d $PACKAGE]
+# Cria o diretório e instala as dependências da função lambda
+if [ -d $PACKAGE ]
 then
-    echo   "O Diretorio "$PACKAGE" já existe."
+	echo "O Diretório "$PACKAGE" já existe."
 else
-    echo "====================================="
-    echo "Criando o diretorio "$PACKAGE"..."
-    mkdir $PACKAGE
-    echo "O diretorio "$PACKAGE" foi criado."
-    echo "====================================="
+	echo "============================================="
+	echo "Criando o diretório "$PACKAGE"..."
+	mkdir $PACKAGE
+	echo "O diretório "$PACKAGE" foi criado."
+	echo "============================================="
 fi
 
-# Declara variavel que localiza o requirements com as dependencias do projeto
+# Declara variavel que localiza o requirements com as dependências do projeto
 FILE_REQUIREMENTS=../etl/lambda_requirements.txt
 
 # Verifica se o arquivo lambda_requirements existe
-if [ -d $FILE_REQUIREMENTS]
+if [ -f $FILE_REQUIREMENTS ]
 then
-    echo "====================================="
-    echo "Instalando dependencias localizadas no "$FILE_REQUIREMENTS""
-    pip install --target ./package -r $FILE_REQUIREMENTS
-    echo "Dependencias intaladas com sucesso."
-    echo "====================================="
+	echo "============================================="
+	echo "Instalando dependências localizadas no "$FILE_REQUIREMENTS""
+	pip install --target ./package -r $FILE_REQUIREMENTS
+	echo "Dependências instaladas com sucesso."
+	echo "============================================="	
 fi
+
 
 cd $PACKAGE
 
-# Declara variavel que localiza a função lambda para reutilização no codigo
-LAMBDA_FUNCTION=../etl/lambda_function.py
+# Declara variavel que localiza a função lambda para reutilização no código.
+LAMBDA_FUNCTION=../../etl/lambda_function.py
 
-# Verifica se o arquivo lambda_requirements existe
-if [ -f $LAMBDA_FUNCTION]
+# Verifica se o arquivo lambda_function.py existe
+if [ -f $LAMBDA_FUNCTION ]
 then
-    echo "====================================="
-    echo "Copiando função Handler..."
-    cp $LAMBDA_FUNCTION .
-    echo "Compactando arquivo lambda_function_payload.zip"
-    zip -r9 ../lambda_function_payload.zip .#Compacta o pacote para o deploy
-    pip install --target ./package -r $FILE_REQUIREMENTS
-    echo "Arquivo compactado com sucesso."
-    echo "====================================="
+	echo "============================================="
+	echo "Copiando função Handler..."
+	cp $LAMBDA_FUNCTION .
+	echo "Compactando arquivo lambda_function_payload.zip"
+	zip -r9 ../lambda_function_payload.zip . #Compacta o pacote para o deploy
+	echo "Arquivo compactado com sucesso!"
+	echo "============================================="
 fi
 
 cd ..
-
-
-
